f141128e8d3ba5d3fb253b537b3470af9e945737
Spencer Hansen <spencerhansen@gmail.com>
2023-03-02 09:21:41 -0700

    finished the interface

---
+++ Movie Library/IMovieRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movie_Library
+{
+    public interface IMovieRepository
+    {
+        IEnumerable<Movie> GetAllMovies();
+        void InsertMovie(int movieid, string movie_name, string rating, string genre, int rating_scale, string movie_or_series);
+        void DeleteMovie(int movieid);
+    }
+}
---
+++ Movie Library/Movie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movie_Library
+{
+    public class Movie
+    {
+        public int movieId { get; set; }
+        public string movie_name { get; set; }
+        public string rating { get; set;}
+        public string genre { get; set;}
+        public int rating_scale { get; set;}
+        public string series_or_movie { get; set;}
+    }
+}
