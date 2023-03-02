afd8a3eb4196f5abb88895714596c9f83ab0da65
Spencer Hansen <spencerhansen@gmail.com>
2023-03-02 09:35:52 -0700

    made sql connection and selected all from datebase Movie_Library

---
+++ Movie Library/DapperMovieRepository.cs
@@ -0,0 +1,32 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movie_Library
+{
+    public class DapperMovieRepository : IMovieRepository
+    {
+        private readonly IDbConnection _conn;
+        public DapperMovieRepository(IDbConnection conn)
+        {
+            _conn = conn;
+        }
+        public IEnumerable<Movie> GetAllMovies()
+        {
+            return _conn.Query<Movie>("select * from movie_library");
+        }
+
+        public void InsertMovie(int movieid, string movie_name, string rating, string genre, int rating_scale, string movie_or_series)
+        {
+            throw new NotImplementedException();
+        }
+        public void DeleteMovie(int movieid)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Movie Library/Program.cs
+++ Movie Library/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿using Microsoft.Extensions.Configuration;
+using Movie_Library;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+string connString = config.GetConnectionString("DefaultConnection");
+
+IDbConnection conn = new MySqlConnection(connString);
+
+var movieRepo = new DapperMovieRepository(conn);
+var movie = movieRepo.GetAllMovies();
+
+foreach(var mov in movie)
+{
+    Console.WriteLine($"{mov.movieId} | {mov.movie_name} | {mov.rating} | {mov.genre} | {mov.rating_scale} | {mov.series_or_movie}");
+}
